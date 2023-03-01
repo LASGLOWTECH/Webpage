c245f614b0b7f2d7984e6dce2f3fe25960770f5b
LASGLOWTCH <omozemojeau@gmail.com>
2023-03-01 04:04:35 +0100

    made changes

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -5,7 +5,7 @@ import emailjs from '@emailjs/browser';
 
 const ContactUs = () => {
   const form = useRef();
-  const [loading, setIsloading]= useState(false)
+  const [loading, setIsloading] = useState(false)
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -13,18 +13,18 @@ const ContactUs = () => {
     setIsloading(true);
 
     emailjs.sendForm('service_9yexy8p', 'template_cl62k3c', form.current, 'PBTi6vGyx_hHTMRZs')
-    
-    .then((result) => {
-        
-          console.log(result.text);
-          setIsloading(false);
-          alert("sent")
-         
+
+      .then((result) => {
+
+        console.log(result.text);
+        setIsloading(false);
+        alert("<h4>sent</h4>")
+
       }, (error) => {
-          console.log(error.text);
-         ;
+        console.log(error.text);
+        ;
       });
-      e.target.reset()
+    e.target.reset()
   };
 
 
@@ -33,66 +33,66 @@ const ContactUs = () => {
 
     <div className="container-fluid">
 
-<div className=' first-row align-self-center'>
+      <div className=' first-row align-self-center'>
 
-<div className='first-col '>
-
-    
-      <img  className="vatar w-75  " src='/images/avatar.png' alt='avatar' ></img>
-       </div>
+        <div className='first-col '>
 
 
-
-       <div className=' second-col'>
-
-
- <form  className="formally"  ref={form} onSubmit={sendEmail}>
-    < h3 className="welcome">Welcome!</h3>
-<p>Enter details </p>
-    
-<div className="form-group">
- <label for="exampleFormControlInput"></label>
- <input type="text" 
- className="form-control" 
-  id="exampleFormControlInput1"  
-  name="user_name" 
-  placeholder="Name" 
-  
-  ></input></div>
-
-<div className="form-group">
- <label for="exampleFormControlInput"></label>
- <input type="Email" 
- className="form-control" 
- id="exampleFormControlInput1"  
-  name="user_email" 
-  required placeholder="Email"
-  
-  ></input></div>
+          <img className="vatar w-75  " src='/images/avatar.png' alt='avatar' ></img>
+        </div>
 
 
 
-<div className="form-group">
- <label for="exampleFormControlInput"></label>
- <label for="exampleFormControlTextarea1"></label>
-              <textarea class="form-control"   name="message" id="exampleFormControlTextarea1" placeholder="Message" cols=""
+        <div className=' second-col'>
+
+
+          <form className="formally" ref={form} onSubmit={sendEmail}>
+            < h3 className="welcome">Welcome!</h3>
+            <p>Enter details </p>
+
+            <div className="form-group">
+              <label for="exampleFormControlInput"></label>
+              <input type="text"
+                className="form-control"
+                id="exampleFormControlInput1"
+                name="user_name"
+                placeholder="Name"
+
+              ></input></div>
+
+            <div className="form-group">
+              <label for="exampleFormControlInput"></label>
+              <input type="Email"
+                className="form-control"
+                id="exampleFormControlInput1"
+                name="user_email"
+                required placeholder="Email"
+
+              ></input></div>
+
+
+
+            <div className="form-group">
+              <label for="exampleFormControlInput"></label>
+              <label for="exampleFormControlTextarea1"></label>
+              <textarea class="form-control" name="message" id="exampleFormControlTextarea1" placeholder="Message" cols=""
                 rows="3"></textarea></div>
 
-<div className="form-group">
-{ !loading && <input type="submit" className=" px-2 sub-btn mt-2 bg-primary text-light" value="Send" />}
-{ loading && <input type="submit" className=" px-2 sub-btn mt-2 bg-warning text-light" disabled  value="Sending" />}
-  </div>
+            <div className="form-group">
+              {!loading && <input type="submit" className=" px-2 sub-btn mt-2 bg-primary text-light" value="Send" />}
+              {loading && <input type="submit" className=" px-2 sub-btn mt-2 bg-warning text-light" disabled value="Sending" />}
+            </div>
 
 
-    </form>
+          </form>
 
 
-</div>
+        </div>
 
 
 
 
-    </div>
+      </div>
     </div>
 
 
