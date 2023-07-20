548664ab2d36db0a81ad73fed95a32956959792a
LASGLOWTECH <omozemojeau@gmail.com>
2023-07-20 14:58:33 +0100

    ADDED A PACKAGE

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import { useEffect, useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Header';
@@ -8,14 +8,41 @@ import ContactUs from './components/login';
 import Homepage from './components/Home';
 import Footer from './components/Footer';
 import Portfolio from './components/portfolio';
+import { ScaleLoader } from "react-spinners";
 import ScrollTop from './components/scroll';
 // import Userdetails from './components/userdetails';
 
 import Userpage from './components/Projects';
 
 const App=()=>{
+  const [loading, setLoading] = useState(false)
 
-  return (  <Router>
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false)
+    }, 5000);
+  
+ 
+    
+  }, [])
+
+  return ( 
+    <>
+    
+    {loading ?
+    <div className='load-val bg-dark 
+  d-flex flex-column justify-content-center align-items-center'
+     style={{ height: '100vh', width: '100%' }}>
+
+ 
+  <ScaleLoader color='#36d7b7' loading={loading} size='150'/>
+
+
+     </div> :
+    
+
+    <Router>
     <div className='apprender'>
     
       <Navbar/>
@@ -39,9 +66,13 @@ const App=()=>{
 </div>
 
         </Router>
+  }
+      
+      
+   
 
       
-  
+</>
       
       
     
